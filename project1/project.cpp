d6139cc6c48a28e9de627f31cf81d95c9e9fe9d0
Adi Adnyana <adiadnyana22@gmail.com>
2021-01-15 02:08:54 +0800

    struct aj

--- project1/project.cpp
+++ project1/project.cpp
@@ -16,10 +16,35 @@ struct friendReq {
     friendReq *next, *prev;
 };
 
+struct commentNotes {
+    char username[25];
+    char comment[255];
+    commentNotes *next, *prev;
+};
+
+struct recycleBin {
+    char content[255];
+    int category;
+    int like;
+    int privateOrPublic;
+    recycleBin *next, *prev;
+};
+
 struct userList {
     char username[25];
     char password[25];
     friendList *head, *tail;
     friendInbox *head, *tail;
     friendReq *head, *tail;
-};
+    userList *next, *prev;
+} *headUser, *tailUser;
+
+struct note {
+    char username[25];
+    char content[255];
+    int category;
+    int like;
+    int privateOrPublic;
+    commentNotes *head, *tail;
+    note *next, *prev;
+} *headNote, *taillNote;
